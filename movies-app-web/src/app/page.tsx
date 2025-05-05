721a5b2217a967d1357a4cd54d8f09c1c33642dd
Pablo Castelo <pablohenriquec7@gmail.com>
2025-05-04 22:27:51 -0300

    fix: redirect home to login page

--- movies-app-web/src/app/page.tsx
+++ movies-app-web/src/app/page.tsx
@@ -1,16 +1,12 @@
 "use client";
 
-import RouteGuard from "@/components/RouteGuard";
-import { Flex, Text } from "@chakra-ui/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
-	return (
-		<>
-			<RouteGuard>
-				<Flex>
-					<Text>Teste home</Text>
-				</Flex>
-			</RouteGuard>
-		</>
-	);
+	const router = useRouter();
+	useEffect(() => {
+		router.replace("/login");
+	}, []);
+	return <></>;
 }
